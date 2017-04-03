33310b5a86d6e532018647118f0422f9ae4f54e4
tonyandminh <minhdi11@gmail.com>
2017-04-02 23:10:56 -0400

    Read in local json file
    Read in an iterate through the json object

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,24 +1,11 @@
 Pebble.addEventListener("ready", function(event){
-    console.log("Ready");
-    
     //var myWorkoutJSON = JSON.parse();
+   var workoutsJSON = require('./workouts.json');
+    console.log(workoutsJSON.Workouts.Plyometrics.Moves[0].Name);
+    console.log(workoutsJSON.Workouts.Plyometrics.Moves[0].Time);
     
-    var request = new XMLHttpRequest();
-   request.open("GET", "src/js/workouts.json", false);
-   request.send(null);
-   var workoutsJSONObj = JSON.parse(request.responseText);
-   console.log (workoutsJSONObj.Workouts);
-    
-    
-    var text = '{"employees":[' +
-        '{"firstName":"John","lastName":"Doe" },' +
-        '{"firstName":"Anna","lastName":"Smith" },' +
-        '{"firstName":"Peter","lastName":"Jones" }]}';
-    
-    var obj = JSON.parse(text);
-    console.log(obj.employees[1].firstName + " " + obj.employees[1].lastName);
-});
-
-Pebble.addEventListener("appmessage", function(event){
-    console.log("Hello");
+    for(var i = 0; i < workoutsJSON.Workouts.Plyometrics.Moves.length; i++){
+        console.log(workoutsJSON.Workouts.Plyometrics.Moves[i].Name);
+    console.log(workoutsJSON.Workouts.Plyometrics.Moves[i].Time);
+    }
 });
